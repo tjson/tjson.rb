3afc66d6ad39ccac0c312ea3911b6e8064d84685
Tony Arcieri <bascule@gmail.com>
2016-10-16 18:32:20 -0700

    Constrain types allowed as object member names
    Only Unicode Strings and Binary Data are allowed

--- lib/tjson/object.rb
+++ lib/tjson/object.rb
@@ -4,6 +4,14 @@ module TJSON
   # TJSON object type (i.e. hash/dict-alike)
   class Object < ::Hash
     def []=(name, value)
+      unless name.start_with?("u:", "b16:", "b64:")
+        if name.include?(":")
+          raise TJSON::ParseError, "invalid tag on object name: #{name[/\A(.*?):/, 1]}"
+        else
+          raise TJSON::ParseError, "no tag found on object name: #{name.inspect}"
+        end
+      end
+
       super(TJSON::TagParser.parse(name), TJSON::TagParser.parse(value))
     end
   end
--- spec/tjson/object_spec.rb
+++ spec/tjson/object_spec.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe TJSON::Object do
-  it "needs tests!"
+  subject(:object) { described_class.new }
+
+  describe "member name types" do
+    let(:example_value) { "u:bar".dup }
+
+    context "Unicode Strings" do
+      let(:example_name) { "u:foo".dup }
+
+      it "parses successfully" do
+        expect { object[example_name] = example_value }.not_to raise_error
+      end
+    end
+
+    context "Binary Data" do
+      let(:example_name) { "b16:48656c6c6f2c20776f726c6421".dup }
+
+      it "parses successfully" do
+        expect { object[example_name] = example_value }.not_to raise_error
+      end
+    end
+
+    context "types other than Unicode Strings or Binary Data" do
+      let(:example_name) { "i:42".dup }
+
+      it "raises TJSON::ParseError" do
+        expect { object[example_name] = example_value }.to raise_error(TJSON::ParseError)
+      end
+    end
+  end
 end
